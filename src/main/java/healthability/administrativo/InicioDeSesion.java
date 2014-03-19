69cc0676788cb600c8faae974f21842b271f43f0
Marcelo Delgado <mardelmi@gmail.com>
2014-03-19 12:41:32 -0600

    Inicio de sesion visualmente LISTO

--- src/main/java/healthability/administrativo/InicioDeSesion.java
+++ src/main/java/healthability/administrativo/InicioDeSesion.java
@@ -10,10 +10,28 @@ import java.awt.GridBagLayout;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Color;
+import javax.swing.JPasswordField;
 
 public class InicioDeSesion extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField pwdContrasena;
 
 	/**
 	 * Launch the application.
@@ -35,26 +53,85 @@ public class InicioDeSesion extends JFrame {
 	 * Create the frame.
 	 */
 	public InicioDeSesion() {
+		setTitle("HealthABILITY");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 250, 194);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0};
-		gbl_panel.rowHeights = new int[]{0};
-		gbl_panel.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		JLabel lblInicieSesion = new JLabel("Inicie Sesion");
+		lblInicieSesion.setFont(new Font("SansSerif", lblInicieSesion.getFont().getStyle() & ~Font.ITALIC | Font.BOLD, lblInicieSesion.getFont().getSize() + 5));
+		lblInicieSesion.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblInicieSesion, BorderLayout.NORTH);
 		
-		JLabel lblInicioDeSesin = new JLabel("Inicio de Sesi\u00F3n");
-		lblInicioDeSesin.setFont(new Font("Calibri", Font.PLAIN, 20));
-		lblInicioDeSesin.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblInicioDeSesin, BorderLayout.NORTH);
+		JPanel panel_3 = new JPanel();
+		contentPane.add(panel_3, BorderLayout.CENTER);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setColumns(10);
+		
+		pwdContrasena = new JPasswordField();
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setFont(new Font("SansSerif", lblUsuario.getFont().getStyle() & ~Font.BOLD & ~Font.ITALIC, lblUsuario.getFont().getSize()));
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setFont(new Font("SansSerif", lblUsuario.getFont().getStyle() & ~Font.BOLD & ~Font.ITALIC, lblUsuario.getFont().getSize()));
+		
+		JButton btnIniciarSesion = new JButton("Iniciar Sesi\u00F3n");
+		btnIniciarSesion.setFont(new Font("SansSerif", lblUsuario.getFont().getStyle() & ~Font.BOLD & ~Font.ITALIC, lblUsuario.getFont().getSize()));
+		
+		JSeparator separator = new JSeparator();
+		
+		JButton btnRegistrese = new JButton("Reg\u00EDstrese");
+		btnRegistrese.setFont(new Font("SansSerif", lblUsuario.getFont().getStyle() & ~Font.BOLD & ~Font.ITALIC, lblUsuario.getFont().getSize()));
+		GroupLayout gl_panel_3 = new GroupLayout(panel_3);
+		gl_panel_3.setHorizontalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addGap(20)
+					.addComponent(btnRegistrese, GroupLayout.DEFAULT_SIZE, 194, Short.MAX_VALUE)
+					.addGap(20))
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addGap(20)
+					.addGroup(gl_panel_3.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnIniciarSesion, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
+						.addGroup(gl_panel_3.createSequentialGroup()
+							.addGroup(gl_panel_3.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblUsuario)
+								.addComponent(lblContrasea))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panel_3.createParallelGroup(Alignment.LEADING)
+								.addComponent(pwdContrasena, GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
+								.addComponent(txtUsuario, GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE))))
+					.addGap(20))
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(separator, GroupLayout.DEFAULT_SIZE, 214, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		gl_panel_3.setVerticalGroup(
+			gl_panel_3.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_3.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_3.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txtUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblUsuario))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_3.createParallelGroup(Alignment.BASELINE)
+						.addComponent(pwdContrasena, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblContrasea))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnIniciarSesion)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 2, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnRegistrese)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_3.setLayout(gl_panel_3);
 	}
-
 }
