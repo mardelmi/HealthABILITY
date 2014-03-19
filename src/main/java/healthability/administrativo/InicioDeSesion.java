9b5332feb62ba3afcac3766d84b25e1b9f011acc
Marcelo Delgado <mardelmi@gmail.com>
2014-03-19 12:54:25 -0600

    Pequeño arreglo a InicioDeSesion

--- src/main/java/healthability/administrativo/InicioDeSesion.java
+++ src/main/java/healthability/administrativo/InicioDeSesion.java
@@ -56,7 +56,7 @@ public class InicioDeSesion extends JFrame {
 		setTitle("HealthABILITY");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 250, 194);
+		setBounds(100, 100, 270, 354);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -133,5 +133,6 @@ public class InicioDeSesion extends JFrame {
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		panel_3.setLayout(gl_panel_3);
+		pack();
 	}
 }
